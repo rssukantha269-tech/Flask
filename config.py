cb5ecb7bcf9f51d22eb34525befc4920fdd7c4c6
Sukantha R S <rssukantha269@gmail.com>
2026-01-29 22:07:01 +0530

    Update Config class with new secret key and URI

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
-    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb://localhost:27017/lab_booking'
+    SECRET_KEY = "secret123"
+
+    MONGO_URI = "mongodb://localhost:27017/flask_project"
+
+
