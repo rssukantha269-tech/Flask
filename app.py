b99a1eeb7ab43195b225a0f85a3046bc6327efb2
Sukantha R S <rssukantha269@gmail.com>
2026-01-29 22:05:51 +0530

    Refactor app.py for better structure and clarity
    Refactor User class and update user loader function. Improve code organization with comments and change blueprint imports.

--- app.py
+++ app.py
@@ -6,50 +6,74 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# MongoDB
 mongo = PyMongo(app)
 
+# Login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'auth.login'
+login_manager.login_view = "auth.login"
 
+
+# -------------------------------
+# User class
+# -------------------------------
 class User:
     def __init__(self, user_data):
-        self.id = str(user_data['_id'])
-        self.username = user_data['username']
-        self.email = user_data['email']
-        self.password = user_data['password']
-
-    @staticmethod
-    def is_authenticated():
-        return True
-
-    @staticmethod
-    def is_active():
-        return True
-
-    @staticmethod
-    def is_anonymous():
-        return False
+        self.id = str(user_data["_id"])
+        self.username = user_data["username"]
+        self.email = user_data["email"]
+        self.password = user_data["password"]
 
     def get_id(self):
         return self.id
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+
 @login_manager.user_loader
 def load_user(user_id):
     from bson.objectid import ObjectId
-    user_data = mongo.db.users.find_one({'_id': ObjectId(user_id)})
+
+    user_data = mongo.db.users.find_one(
+        {"_id": ObjectId(user_id)}
+    )
+
     if user_data:
         return User(user_data)
     return None
 
-from routes import auth, booking
 
-app.register_blueprint(auth.bp)
-app.register_blueprint(booking.bp)
+# -------------------------------
+# Blueprints
+# -------------------------------
+from routes.auth import bp as auth_bp
+from routes.booking import bp as booking_bp
 
-@app.route('/')
+app.register_blueprint(auth_bp)
+app.register_blueprint(booking_bp)
+
+
+# -------------------------------
+# Home page
+# -------------------------------
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+# -------------------------------
+# Run app
+# -------------------------------
+if __name__ == "__main__":
+    app.run()
